b563a5dc296d809925ae6e45204113f166313d9b
AGATS <vantyz757@gmail.com>
2025-10-01 23:05:47 +0300

    php12

---
+++ test-symfony-project/src/Controller/CarController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CarRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class CarController extends AbstractController
+{
+
+    public function __construct(
+                    private CarRepository $carRepository, 
+                    private EntityManagerInterface $entityManager)
+                  {}
+
+    #[Route('/car', name: 'app_car')]
+    public function index(): Response
+    {
+
+        $allcars = $this->carRepository->findAll();
+
+        $carsafter2015 = $this->carRepository->createQueryBuilder('c')
+        ->where('c.year > :value')
+          ->setParameter('value',2015)
+          ->orderBy('c.id','ASC')
+          ->getQuery()
+          ->getResult();
+
+        return $this->render('car/index.html.twig', [
+            'cars'=>$allcars,
+            'carsafter2015'=>$carsafter2015
+
+        ]);
+    }
+
+}
---
+++ test-symfony-project/src/Controller/MovieController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller;
+
+	use Symfony\Component\HttpFoundation\Request;
+
+use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MovieController extends AbstractController
+{
+
+   private $descriptions = [
+    1 => 'Фильм о том как эльфу искали закуску к пиву',
+    2 => 'Фильм про шикарные пельмеши',
+    3 => 'Фильм про темное прошлое Насти из П-31',
+    4 => 'Фильм про возвращение Насти в РМТ'
+];
+
+    public function __construct(
+                    private MovieRepository $movieRepository, 
+                    private EntityManagerInterface $entityManager)
+                  {}
+
+
+    #[Route('/base', name: 'app_movie_base')]
+    public function base(): Response
+    {
+         $movies = $this->movieRepository->findAll();
+
+        return $this->render('movie/base.html.twig', [
+           'movies'=>$movies 
+        ]);
+    }
+
+
+    #[Route('/movie', name: 'app_movie')]
+    public function index(): Response
+    {
+         $movies = $this->movieRepository->findAll();
+
+
+
+        return $this->render('movie/index.html.twig', [
+            'movies'=>$movies
+        ]);
+    }
+
+    #[Route('/movie/{id}', name: 'app_movie_show')]
+    public function show(int $id): Response
+    {
+        
+         $movie = $this->movieRepository->find($id);
+
+         $movie->description = $this->descriptions[$id] ?? 'Описание отсутствует';
+
+         if (!$movie) {
+        throw $this->createNotFoundException('Фильм с ID ' . $id . ' не найден');
+    }
+
+        return $this->render('movie/show.html.twig', [
+            'movie'=>$movie
+        ]);
+    }
+
+    #[Route('/base/random', name: 'app_movie_random')]
+    public function random(): Response
+    {
+        $movies= $this->movieRepository->findAll();
+        
+
+         $randomKey = array_rand($movies);
+    $randomMovie = $movies[$randomKey];
+    $id = $randomMovie->getId(); // Получаем ID через геттер
+
+
+
+         if (!$randomMovie) {
+        throw $this->createNotFoundException('Фильм не найден');
+    }
+
+        return $this->render('movie/random.html.twig', [
+            'movie'=>$randomMovie,
+            'id'=>$id
+        ]);
+    }
+}
